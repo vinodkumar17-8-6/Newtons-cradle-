94a071357fa1716e76a7564bfd8626791570a3e6
vinodkumar17-8-6 <73932115+vinodkumar17-8-6@users.noreply.github.com>
2020-11-29 09:20:52 -0500

    Add files via upload

---
+++ Bob.js
@@ -0,0 +1,31 @@
+class bob{
+    constructor(x,y,r)
+    {
+        var options=
+        {
+            'isStatic':false,
+            'restitution':1,
+            'friction':1.0,
+            'density':1.2
+        }
+        this.x=x;
+        this.y=y;
+        this.r=r;
+        this.body=Bodies.circle(x,y,this.r,options);
+        World.add(world,this.body);
+        
+     
+        
+    }
+
+    display()
+    {
+        var bobpos=this.body.position;
+        push();
+        fill("blue");
+        translate(bobpos.x,bobpos.y);
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.r,this.r);
+        pop();
+    }
+}
